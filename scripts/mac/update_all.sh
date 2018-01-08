8b45db80916cb221c2ebbfa2c1d093b967873b06
k03mad <k03.mad@gmail.com>
2018-01-08 17:14:23 +0300

    update cleanup

--- scripts/mac/update_all.sh
+++ scripts/mac/update_all.sh
@@ -9,8 +9,9 @@ brew update
 brew upgrade
 brew tap caskroom/cask
 
-___print "npm: install npm-check-updates"
+___print "npm: install packages"
 npm i npm -g
+npm i ttab -g
 npm i npm-check-updates -g
 
 ___print "npm: update global"
@@ -21,3 +22,19 @@ do
     echo $package
     npm i "$package" -g
 done
+
+___print "NOW CLEANUP!"
+
+___print "remove all .DS_Store and Icon? files"
+find . -type f -name '*.DS_Store' -print -delete
+find . -type f -name 'Icon?' -print -delete
+
+___print "eject all disks"
+osascript -e 'tell application "Finder" to eject (every disk whose ejectable is true)'
+
+___print "clear trash and logs"
+sudo rm -rfv /Volumes/*/.Trashes; sudo rm -rfv ~/.Trash; sudo rm -rfv /private/var/log/asl/*.asl; sqlite3 ~/Library/Preferences/com.apple.LaunchServices.QuarantineEventsV* 'delete from LSQuarantineEvent'
+
+___print "brew cleanup"
+brew cleanup
+brew cask cleanup
