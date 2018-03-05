ec272efbc4c436b9449467354f49a1c7507e911b
Kirill Molchanov <k03.mad@gmail.com>
2018-03-05 23:19:25 +0300

    Update run_bots.sh

--- scripts/run_bots.sh
+++ scripts/run_bots.sh
@@ -4,18 +4,13 @@ function run {
     echo -e "\033[0;33m🤖  RUNNING $1 \033[0m"
     cd /home/pi/git/$1/
     git pull
-    nvm install
     npm run setup
     npm run bot
 }
 
-if [ -z "$(forever)" ]; then
-    npm i -g forever
-fi
-
 run "yet-another-tlgrm-bot"
 run "raspi-tlgrm-bot"
-# # run "grammar-tlgrm-bot"
+# run "grammar-tlgrm-bot"
 
 # reboot pi after 12h
 sudo shutdown --no-wall -r +720
