460ba529212a507666f1c1efd65b12d01a88a3bf
k03mad <k03.mad@gmail.com>
2018-10-01 21:53:25 +0300

    sml fix

--- scripts/tools/zsh.sh
+++ scripts/tools/zsh.sh
@@ -2,7 +2,7 @@
 
 git clone https://github.com/lukechilds/zsh-nvm $ZSH_CUSTOM/plugins/zsh-nvm
 git clone https://github.com/TamCore/autoupdate-oh-my-zsh-plugins $ZSH_CUSTOM/plugins/autoupdate
-git clone https://github.com/zdharma/fast-syntax-highlighting.git $ZSH_CUSTOM/plugins/fast-syntax-highlighting
+git clone https://github.com/zdharma/fast-syntax-highlighting $ZSH_CUSTOM/plugins/fast-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
 git clone https://github.com/supercrabtree/k $ZSH_CUSTOM/plugins/k
 
