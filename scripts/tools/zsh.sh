89a1ef961acc7be1e904b23dae3032f5898fb534
k03mad <k03.mad@gmail.com>
2018-10-01 21:53:43 +0300

    sort lines

--- scripts/tools/zsh.sh
+++ scripts/tools/zsh.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 git clone https://github.com/lukechilds/zsh-nvm $ZSH_CUSTOM/plugins/zsh-nvm
+git clone https://github.com/supercrabtree/k $ZSH_CUSTOM/plugins/k
 git clone https://github.com/TamCore/autoupdate-oh-my-zsh-plugins $ZSH_CUSTOM/plugins/autoupdate
 git clone https://github.com/zdharma/fast-syntax-highlighting $ZSH_CUSTOM/plugins/fast-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
-git clone https://github.com/supercrabtree/k $ZSH_CUSTOM/plugins/k
 
 curl https://raw.githubusercontent.com/scopatz/nanorc/master/install.sh | sh
