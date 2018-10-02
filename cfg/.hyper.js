4658035b6ee3b453607b58f51f5dfbfc48846d29
k03mad <k03.mad@gmail.com>
2018-10-02 18:24:47 +0300

    update hyper conf

--- cfg/.hyper.js
+++ cfg/.hyper.js
@@ -1,19 +1,20 @@
 module.exports = {
     config: {
-        updateChannel: 'stable',
-        fontSize: 12,
-        fontFamily: '"Fira Code", Menlo, "DejaVu Sans Mono", "Lucida Console", monospace',
-        cursorShape: 'BEAM',
-        cursorBlink: true,
         backgroundColor: '#263238',
+        cursorBlink: true,
+        cursorShape: 'BEAM',
+        fontFamily: '"Fira Code", Menlo, "DejaVu Sans Mono", "Lucida Console", monospace',
+        fontSize: 12,
         padding: '12px 14px',
+        scrollback: 10000,
         shell: 'zsh',
         shellArgs: ['--login'],
+        updateChannel: 'canary',
     },
     plugins: [
-        'hyperterm-material-theme',
         'hyper-final-say',
         'hyper-hide-scroll',
         'hyper-hide-title',
+        'hyperterm-material-theme',
     ],
 };
