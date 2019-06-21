b64185794768a39177a28ed25f4f257d5b8371b6
k03mad <k03.mad@gmail.com>
2019-06-21 15:59:04 +0300

    remove some npm global tools

--- scripts/tools/npm.sh
+++ scripts/tools/npm.sh
@@ -3,13 +3,10 @@
 npmTools=(
     forever
     fx
-    gtop
     http-server
     jay-repl
     npm-check-updates
     pure-prompt
-    rename-cli
-    selenium-standalone
     speed-test
     start-ios-simulator
 )
@@ -18,5 +15,3 @@ for i in "${npmTools[@]}"
 do
     npm i "$i" -g
 done
-
-selenium-standalone install
