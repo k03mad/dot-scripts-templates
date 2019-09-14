f2a1f732481a652bf75b2d22a49ab69a0d0816c1
k03mad <k03.mad@gmail.com>
2019-09-15 00:08:50 +0300

    betterbrew

--- scripts/tools/zsh.sh
+++ scripts/tools/zsh.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+git clone https://github.com/djui/alias-tips.git $ZSH_CUSTOM/plugins/alias-tips
 git clone https://github.com/lukechilds/zsh-nvm $ZSH_CUSTOM/plugins/zsh-nvm
 git clone https://github.com/TamCore/autoupdate-oh-my-zsh-plugins $ZSH_CUSTOM/plugins/autoupdate
+git clone https://github.com/timothyrowan/betterbrew-zsh-plugin.git $ZSH_CUSTOM/plugins/betterbrew
 git clone https://github.com/zdharma/fast-syntax-highlighting $ZSH_CUSTOM/plugins/fast-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
-git clone https://github.com/djui/alias-tips.git $ZSH_CUSTOM/plugins/alias-tips
 
 curl https://raw.githubusercontent.com/scopatz/nanorc/master/install.sh | sh
