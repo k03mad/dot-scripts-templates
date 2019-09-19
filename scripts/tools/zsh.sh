3aef920785a7204efa783a3ef6478ea19201bc79
Kirill Molchanov <k03.mad@gmail.com>
2019-09-20 01:19:48 +0300

    remove unused

--- scripts/tools/zsh.sh
+++ scripts/tools/zsh.sh
@@ -3,7 +3,6 @@
 git clone https://github.com/djui/alias-tips.git $ZSH_CUSTOM/plugins/alias-tips
 git clone https://github.com/lukechilds/zsh-nvm $ZSH_CUSTOM/plugins/zsh-nvm
 git clone https://github.com/TamCore/autoupdate-oh-my-zsh-plugins $ZSH_CUSTOM/plugins/autoupdate
-git clone https://github.com/timothyrowan/betterbrew-zsh-plugin.git $ZSH_CUSTOM/plugins/betterbrew
 git clone https://github.com/zdharma/fast-syntax-highlighting $ZSH_CUSTOM/plugins/fast-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
 
