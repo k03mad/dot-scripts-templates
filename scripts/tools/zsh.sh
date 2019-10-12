7670bb334d86c4682187a7ea0623225f94c6587a
k03mad <k03.mad@gmail.com>
2019-10-12 13:53:42 +0300

    update alias

--- scripts/tools/zsh.sh
+++ scripts/tools/zsh.sh
@@ -2,7 +2,6 @@
 
 ZSH_CUSTOM="${ZSH}/custom"
 
-git clone https://github.com/djui/alias-tips "${ZSH_CUSTOM}/plugins/alias-tips"
 git clone https://github.com/lukechilds/zsh-nvm "${ZSH_CUSTOM}/plugins/zsh-nvm"
 git clone https://github.com/TamCore/autoupdate-oh-my-zsh-plugins "${ZSH_CUSTOM}/plugins/autoupdate"
 git clone https://github.com/zdharma/fast-syntax-highlighting "${ZSH_CUSTOM}/plugins/fast-syntax-highlighting"
