c9d1f6dbb597b05693165f52166faa1c77299119
k03mad <k03.mad@gmail.com>
2019-10-12 14:01:08 +0300

    update brew alias

--- scripts/tools/brew.sh
+++ scripts/tools/brew.sh
@@ -3,28 +3,19 @@
 termTools=(
     aria2
     bat
-    ccat
-    coreutils
     curl
-    exa
     git
-    httpie
     id3v2
     jq
     m-cli
-    mas
     mc
-    micro
     mkvdts2ac3
     mosh
     nano
     ncdu
-    peco
-    ranger
     shellcheck
     tldr
     topgrade
-    tree
     wget
     zsh
     zsh-completions
