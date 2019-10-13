e51de925063ef08020aabf6e535eddd4bf7f9adf
k03mad <k03.mad@gmail.com>
2019-10-13 14:56:34 +0300

    add npm comp

--- scripts/tools/zsh.sh
+++ scripts/tools/zsh.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 
-ZSH_CUSTOM="${ZSH}/custom"
-
-git clone https://github.com/lukechilds/zsh-nvm "${ZSH_CUSTOM}/plugins/zsh-nvm"
-git clone https://github.com/TamCore/autoupdate-oh-my-zsh-plugins "${ZSH_CUSTOM}/plugins/autoupdate"
-git clone https://github.com/zdharma/fast-syntax-highlighting "${ZSH_CUSTOM}/plugins/fast-syntax-highlighting"
-git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM}/plugins/zsh-autosuggestions"
-git clone https://github.com/zsh-users/zsh-completions "${ZSH_CUSTOM}/plugins/zsh-completions"
+git clone https://github.com/lukechilds/zsh-better-npm-completion "${ZSH_PLUGINS}/zsh-better-npm-completion"
+git clone https://github.com/lukechilds/zsh-nvm "${ZSH_PLUGINS}/zsh-nvm"
+git clone https://github.com/TamCore/autoupdate-oh-my-zsh-plugins "${ZSH_PLUGINS}/autoupdate"
+git clone https://github.com/zdharma/fast-syntax-highlighting "${ZSH_PLUGINS}/fast-syntax-highlighting"
+git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_PLUGINS}/zsh-autosuggestions"
+git clone https://github.com/zsh-users/zsh-completions "${ZSH_PLUGINS}/zsh-completions"
 
 curl https://raw.githubusercontent.com/scopatz/nanorc/master/install.sh | sh
