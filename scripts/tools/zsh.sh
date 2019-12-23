513720f4b0f7bff04e53df463267587469451b0e
k03mad <k03.mad@gmail.com>
2019-12-23 18:25:41 +0300

    plugins

--- scripts/tools/zsh.sh
+++ scripts/tools/zsh.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-git clone https://github.com/lukechilds/zsh-better-npm-completion "${ZSH_PLUGINS}/zsh-better-npm-completion"
-git clone https://github.com/lukechilds/zsh-nvm "${ZSH_PLUGINS}/zsh-nvm"
-git clone https://github.com/TamCore/autoupdate-oh-my-zsh-plugins "${ZSH_PLUGINS}/autoupdate"
-git clone https://github.com/zdharma/fast-syntax-highlighting "${ZSH_PLUGINS}/fast-syntax-highlighting"
-git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_PLUGINS}/zsh-autosuggestions"
-git clone https://github.com/zsh-users/zsh-completions "${ZSH_PLUGINS}/zsh-completions"
+git clone https://github.com/lukechilds/zsh-better-npm-completion "${ZSH_CUSTOM}/plugins/zsh-better-npm-completion"
+git clone https://github.com/lukechilds/zsh-nvm "${ZSH_CUSTOM}/plugins/zsh-nvm"
+git clone https://github.com/TamCore/autoupdate-oh-my-zsh-plugins "${ZSH_CUSTOM}/plugins/autoupdate"
+git clone https://github.com/zdharma/fast-syntax-highlighting "${ZSH_CUSTOM}/plugins/fast-syntax-highlighting"
+git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM}/plugins/zsh-autosuggestions"
+git clone https://github.com/zsh-users/zsh-completions "${ZSH_CUSTOM}/plugins/zsh-completions"
 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k "${ZSH_CUSTOM}/themes/powerlevel10k"
 
