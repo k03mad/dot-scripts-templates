c4bf2956d6bcb14d11ef7130451f57da9e297046
k03mad <k03.mad@gmail.com>
2020-08-02 05:32:33 +0300

    typo

--- scripts/tools/apt.sh
+++ scripts/tools/apt.sh
@@ -28,9 +28,9 @@ aptTools=(
 
 for i in "${aptTools[@]}"
 do
-    # if [ -z "${NO_SUDO}" ]; then
-    #     apt-get install "$i"
-    # else
+    if [ -z "${NO_SUDO}" ]; then
         sudo apt-get install "$i"
-    # fi
+    else
+        apt-get install "$i"
+    fi
 done
