f2db30089c0cd78fb11afa128c72c4c913ad4af9
k03mad <k03.mad@gmail.com>
2021-05-05 21:20:10 +0300

    npmin all

--- tools/npm.sh
+++ tools/npm.sh
@@ -18,12 +18,4 @@ npmTools=(
     speed-test
 )
 
-for i in "${npmTools[@]}"
-do
-    printf "\n ✨ %s \n" "$i"
-    if npm list -g "$i" > /dev/null 2>&1; then
-        printf " ✅ already installed \n"
-    else
-        npm i -g "$i"
-    fi
-done
+npm i "${npmTools[@]}" -g
