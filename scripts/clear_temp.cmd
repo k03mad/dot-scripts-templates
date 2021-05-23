70aefe34bfbc820d70c1027718efc4945e3d6def
k03mad <k03.mad@gmail.com>
2021-05-24 02:56:08 +0300

    remove clear temp

--- scripts/clear_temp.cmd
+++
@@ -1,8 +0,0 @@
-rem remove some temp dirs
-
-for /d %%x in (
-	C:\Temp D:\Temp %TMP% %TEMP% %SystemRoot%\Temp "%LOCALAPPDATA%\Microsoft\Windows\Temporary Internet Files"
-) do (
-	del %%x\* /F /Q
-	for /d %%p in (%%x\*) do rd /Q /S "%%p"
-)
