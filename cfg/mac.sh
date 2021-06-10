05110dc1f5843b2901ef1623e3d1ad6f20055549
kirill-m <kirill-m@yandex-team.ru>
2021-06-10 14:37:16 +0300

    update mac.sh

--- cfg/mac.sh
+++ cfg/mac.sh
@@ -4,126 +4,90 @@ function ___print {
     echo -e "\\033[0;33m🤖  $1 \\033[0m"
 }
 
-___print "git: push current branch"
-git config --global push.default current
-
-___print "system: close any open system preferences panes"
+___print "quit prefs"
 osascript -e 'tell application "System Preferences" to quit'
 
-___print "system: set ntp server"
+___print "ntp"
 m ntp enable
 m ntp set ru.pool.ntp.org
 
-# ___print "system: mojave font fix"
-# defaults write -g CGFontRenderingFontSmoothingDisabled -bool FALSE
+___print "dock"
+m dock position LEFT
+m dock autohide NO
 
-___print "system: disable disk image verification"
+___print "disable disk image verification"
 defaults write com.apple.frameworks.diskimages skip-verify -bool true
 defaults write com.apple.frameworks.diskimages skip-verify-locked -bool true
 defaults write com.apple.frameworks.diskimages skip-verify-remote -bool true
 
-___print "system: download all updates daily"
+___print "download all updates daily"
 defaults write com.apple.SoftwareUpdate AutomaticCheckEnabled -bool true
 defaults write com.apple.SoftwareUpdate ScheduleFrequency -int 1
 defaults write com.apple.SoftwareUpdate AutomaticDownload -int 1
 defaults write com.apple.SoftwareUpdate CriticalUpdateInstall -int 1
 defaults write com.apple.commerce AutoUpdate -bool true
 
-___print "system: automatically quit printer app once the print jobs complete"
+___print "automatically quit printer app once the print jobs complete"
 defaults write com.apple.print.PrintingPrefs "Quit When Finished" -bool true
 
-___print "trash: disable the crash reporter"
+___print "disable the crash reporter"
 defaults write com.apple.CrashReporter DialogType -string "none"
 
-___print "trash: avoid creating .DS_Store files on network or usb"
+___print "avoid creating .DS_Store files on network or usb"
 defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
 defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
 
-___print "trash: disable the warning before emptying the trash"
+___print "disable the warning before emptying the trash"
 defaults write com.apple.finder WarnOnEmptyTrash -bool false
 
-___print "finder: expand save panel by default"
+___print "expand save panel by default"
 defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
 defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode2 -bool true
 
-___print "finder: disable the 'are you sure you want to open this application?' dialog"
+___print "disable the 'are you sure you want to open this application?' dialog"
 defaults write com.apple.LaunchServices LSQuarantine -bool false
 
-___print "finder: show hidden files and filenames"
+___print "show hidden files and filenames"
 defaults write com.apple.finder AppleShowAllFiles -bool true
 defaults write NSGlobalDomain AppleShowAllExtensions -bool true
 
-___print "finder: keep folders on top when sorting by name"
+___print "keep folders on top when sorting by name"
 defaults write com.apple.finder _FXSortFoldersFirst -bool true
 
-___print "finder: disable the warning when changing a file extension"
+___print "disable the warning when changing a file extension"
 defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
 
-___print "finder: enable snap-to-grid for icons on the desktop and in other icon views"
+___print "enable snap-to-grid for icons"
 /usr/libexec/PlistBuddy -c "Set :DesktopViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
 /usr/libexec/PlistBuddy -c "Set :FK_StandardViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
 /usr/libexec/PlistBuddy -c "Set :StandardViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
 
-___print "finder: use list view in all windows by default"
+___print "use list view in all windows by default"
 defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
 
-___print "finder: show the ~/Library and /Volumes folder"
+___print "show the ~/Library and /Volumes folder"
 chflags nohidden ~/Library
 sudo chflags nohidden /Volumes
 
-___print "finder: prevent photos from opening automatically when devices are plugged in"
+___print "prevent photos from opening automatically when devices are plugged in"
 defaults -currentHost write com.apple.ImageCapture disableHotPlug -bool true
 
-___print "type: disable character accent after key long press"
+___print "disable character accent after key long press"
 defaults write -g ApplePressAndHoldEnabled -bool false
 
-___print "type: disable smart quotes and dashes"
+___print "disable smart quotes and dashes"
 defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
 defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
 
-___print "type: enable full keyboard access for all controls"
+___print "enable full keyboard access for all controls"
 defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
 
-___print "trackpad: enable tap to click"
+___print "enable tap to click"
 defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
 defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
 defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
 
-___print "safari: don't send search queries to apple"
-defaults write com.apple.Safari UniversalSearchEnabled -bool false
-defaults write com.apple.Safari SuppressSearchSuggestions -bool true
-
-___print "safari: show the full url in the address bar"
-defaults write com.apple.Safari ShowFullURLInSmartSearchField -bool true
-
-___print "safari: set home page to about:blank"
-defaults write com.apple.Safari HomePage -string "about:blank"
-
-___print "safari: enable debug and develop menu"
-defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
-defaults write com.apple.Safari IncludeDevelopMenu -bool true
-defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
-defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled -bool true
-
-___print "transmission: trash original torrent files"
-defaults write org.m0k.transmission DeleteOriginalTorrent -bool true
-___print "transmission: hide the donate message"
-defaults write org.m0k.transmission WarningDonate -bool false
-___print "transmission: hide the legal disclaimer"
-defaults write org.m0k.transmission WarningLegal -bool false
-
-# ___print "desktop: screen saver at top right screen corner"
-# defaults write com.apple.dock wvous-tr-corner -int 5
-# defaults write com.apple.dock wvous-tr-modifier -int 0
-
-# ___print "desktop: disable notification center and remove the menu bar icon"
-# launchctl unload -w /System/Library/LaunchAgents/com.apple.notificationcenterui.plist 2> /dev/null
-
-___print "dock: left side without autohide"
-m dock position LEFT
-m dock autohide NO
-
 ___print "done: kill affected applications"
-for app in "Dock" "Finder" "Photos" "Safari" "SystemUIServer" "Transmission"; do
+for app in "Dock" "Finder" "Photos" "SystemUIServer"; do
     killall "${app}" &> /dev/null
 done
