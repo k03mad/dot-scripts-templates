c079fe3de99677cd19647de056142a3668e6249e
kirill-m <kirill-m@yandex-team.ru>
2021-06-11 19:32:53 +0300

    add zshup

--- posh/profile.ps1
+++ posh/profile.ps1
@@ -2,3 +2,12 @@ oh-my-posh --init --shell pwsh --config ~/git/dot-scripts-templates/posh/mad.omp
 
 Import-Module "$($(Get-Item $(Get-Command scoop).Path).Directory.Parent.FullName)\modules\scoop-completion"
 fnm env --use-on-cd | Out-String | Invoke-Expression
+
+Function UpdateDot {
+    $current = $pwd
+    cd ~\git\dot-scripts-templates
+    git pull
+    cd $current
+}
+
+Set-Alias -Name zshup -Value UpdateDot
