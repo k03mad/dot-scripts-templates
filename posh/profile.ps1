0399baf4c7e5aea2b9650c8b414747804ad0c9a1
kirill-m <kirill-m@yandex-team.ru>
2021-06-11 18:55:14 +0300

    scoop upd

--- posh/profile.ps1
+++ posh/profile.ps1
@@ -1,3 +1,4 @@
 oh-my-posh --init --shell pwsh --config ~/git/dot-scripts-templates/posh/mad.omp.json | Invoke-Expression
 
+Import-Module "$($(Get-Item $(Get-Command scoop).Path).Directory.Parent.FullName)\modules\scoop-completion"
 fnm env --use-on-cd | Out-String | Invoke-Expression
--- posh/scoop.ps1
+++ posh/scoop.ps1
@@ -1,14 +1,23 @@
 scoop update
 scoop update *
 
-scoop bucket add nerd-fonts
+$buckets = @(
+    'extras',
+    'nerd-fonts',
+    'scoop-completion https://github.com/Moeologist/scoop-completion',
+    'wsl https://git.irs.sh/KNOXDEV/wsl'
+)
+
+$buckets | ForEach-Object {scoop bucket add $PSItem}
 
 $tools = @(
+    'aria2',
     'curl',
     'firacode-nf',
     'fnm',
     'git',
     'nano'
+    'scoop-completion',
     'https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/oh-my-posh.json'
 )
 
