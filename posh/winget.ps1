252ba0cbb2c44e9b9cbf8600ffc180b2f5dcd93c
k03mad <k03.mad@gmail.com>
2021-06-24 20:24:51 +0300

    update ps

--- posh/winget.ps1
+++ posh/winget.ps1
@@ -1,8 +1,23 @@
 $uninstall = @(
-    'extras',
-    'nerd-fonts',
-    'scoop-completion https://github.com/Moeologist/scoop-completion',
-    'wsl https://git.irs.sh/KNOXDEV/wsl'
+    # Проверка работоспособности ПК Windows
+    '{DAA78B45-828D-4E8C-848C-50A70ACC6747}',
+    # Cortana
+    'Microsoft.549981C3F5F10_8wekyb3d8bbwe'
+    # Xbox
+    'Microsoft.GamingApp_8wekyb3d8bbwe'
+    'Microsoft.GetHelp_8wekyb3d8bbwe'
+    'Microsoft.HEIFImageExtension_8wekyb3d8bbwe',
+    'Microsoft.People_8wekyb3d8bbwe',
+    'Microsoft.PowerAutomateDesktop_8wekyb3d8bbwe',
+    'Microsoft.Windows.Photos_8wekyb3d8bbwe',
+    'Microsoft.WindowsAlarms_8wekyb3d8bbwe',
+    'Microsoft.WindowsCamera_8wekyb3d8bbwe',
+    'Microsoft.WindowsMaps_8wekyb3d8bbwe'
+    'Microsoft.XboxGameOverlay_8wekyb3d8bbwe'
+    'Microsoft.XboxGamingOverlay_8wekyb3d8bbwe'
+    'Microsoft.XboxIdentityProvider_8wekyb3d8bbwe',
+    'Microsoft.XboxSpeechToTextOverlay_8wekyb3d8bbwe',
+    'Microsoft.YourPhone_8wekyb3d8bbwe',
 )
 
 $uninstall | ForEach-Object {
