9a6f8f0fce2c1b7ba60dc6585ff2a8760f0f45e0
k03mad <k03.mad@gmail.com>
2021-10-31 21:06:52 +0300

    remove fast-syntac-hl

--- zsh/scripts/zsh.sh
+++ zsh/scripts/zsh.sh
@@ -4,9 +4,9 @@ git clone https://github.com/lukechilds/zsh-better-npm-completion "${ZSH_CUSTOM}
 git clone https://github.com/lukechilds/zsh-nvm "${ZSH_CUSTOM}/plugins/zsh-nvm"
 git clone https://github.com/MichaelAquilina/zsh-you-should-use.git "${ZSH_CUSTOM}/plugins/you-should-use"
 git clone https://github.com/TamCore/autoupdate-oh-my-zsh-plugins "${ZSH_CUSTOM}/plugins/autoupdate"
-git clone https://github.com/zdharma/fast-syntax-highlighting "${ZSH_CUSTOM}/plugins/fast-syntax-highlighting"
 git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM}/plugins/zsh-autosuggestions"
 git clone https://github.com/zsh-users/zsh-completions "${ZSH_CUSTOM}/plugins/zsh-completions"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting "${ZSH_CUSTOM}/plugins/zsh-syntax-highlighting"
 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k "${ZSH_CUSTOM}/themes/powerlevel10k"
 
