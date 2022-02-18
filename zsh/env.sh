3cddf302b7413898c3c3e476c9dde29ddfd13621
kirill-m <kirill-m@yandex-team.ru>
2022-02-18 12:03:20 +0300

    add gitconfig

--- zsh/env.sh
+++ zsh/env.sh
@@ -1,4 +1,5 @@
 export DOT_FOLDER="${HOME}/git/dot-scripts-templates"
+export DOT_FOLDER_EXTRA="${DOT_FOLDER}/extra"
 export DOT_FOLDER_ZSH="${DOT_FOLDER}/zsh"
 export DOT_FOLDER_ZSH_SCRIPTS="${DOT_FOLDER}/zsh/scripts"
 
@@ -7,6 +8,7 @@ export ZSH_CUSTOM="${ZSH}/custom"
 export ZSH_CUSTOM_PLUGINS="${ZSH_CUSTOM}/plugins"
 export ZSH_CUSTOM_THEMES="${ZSH_CUSTOM}/themes"
 
+export GIT_CONFIG_GLOBAL="${DOT_FOLDER_EXTRA}/.gitconfig"
 export FZF_DEFAULT_OPTS="--info=hidden --prompt="
 export NVM_LAZY_LOAD=true
 
