72d91c914e042c9af51ee0de5eb4c32e3cf759f1
k03mad <k03.mad@gmail.com>
2025-01-07 01:37:33 +0300

    refactor

---
+++ zsh/_index.zsh
@@ -0,0 +1,15 @@
+# shellcheck source=/dev/null disable=SC2154
+
+dir=${funcsourcetrace[1]%/*}
+
+source "${dir}/env.zsh"
+source "${HOME}/.env.local.zsh" 2> /dev/null
+source "${HOME}/.env.work.zsh" 2> /dev/null
+
+source "${dir}/alias.zsh"
+source "${dir}/func.zsh"
+
+source "${dir}/omz.zsh"
+source "${dir}/p10k.zsh"
+
+source "${dir}/start.zsh"
