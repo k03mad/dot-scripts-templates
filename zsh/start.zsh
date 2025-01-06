8437f5b90320fb9e0195e6621f5cfd06fa476097
k03mad <k03.mad@gmail.com>
2025-01-07 01:51:44 +0300

    update pi start

--- zsh/start.zsh
+++ zsh/start.zsh
@@ -1,5 +1,5 @@
-if [ -d "${GIT_FOLDER}/lan951x-led-ctl" ]; then
-    sudo sh -c 'echo 0 > /sys/class/leds/PWR/brightness'
-    sudo sh -c 'echo 0 > /sys/class/leds/ACT/brightness'
+if [ -d "${GIT_FOLDER}/lan951x-led-ctl" ] && [ "$(cat /sys/class/leds/PWR/brightness)" -ne 0 ]; then
+    sudo bash -c 'echo 0 > /sys/class/leds/PWR/brightness'
+    sudo bash -c 'echo 0 > /sys/class/leds/ACT/brightness'
     sudo "${GIT_FOLDER}/lan951x-led-ctl/lan951x-led-ctl" --fdx=0 --lnk=0 --spd=0 > /dev/null
 fi
