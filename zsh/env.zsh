bbb22511850f7de493a862cde55c9e0c30e0790b
k03mad <k03.mad@gmail.com>
2025-09-19 18:47:41 +0300

    remove unused

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -25,35 +25,6 @@ export HOMEBREW_NO_ANALYTICS=1
 export HOMEBREW_NO_ENV_HINTS=1
 export ET_NO_TELEMETRY=1
 
-export PATH="\
-/data/data/com.termux/files/usr/bin:\
-/data/data/com.termux/files/usr/bin/applets:\
-/opt/homebrew/bin:\
-/opt/homebrew/sbin:\
-/opt/homebrew/opt/curl/bin:\
-/sbin:\
-/usr/local/bin:\
-/usr/local/go/bin:\
-/usr/local/opt/curl/bin:\
-/usr/local/opt/findutils/libexec/gnubin:\
-/usr/local/sbin:\
-/usr/sbin:\
-${HOME}/.cargo/bin:\
-${HOME}/.local/bin:\
-${HOME}/arcadia:\
-${HOME}/bin:\
-${HOME}/go/bin:\
-${HOME}/.gvm/go/bin:\
-${M2_HOME}/bin:\
-${PATH}\
-"
-
-export FPATH="\
-${HOME}/.zfunc:\
-${ZSH_CUSTOM_PLUGINS}/zsh-completions/src:\
-${FPATH}\
-"
-
 if [ -n "${TERMUX_VERSION}" ]; then
     export SKIP_NVMRC="true"
     export PUPPETEER_SKIP_CHROMIUM_DOWNLOAD="true"
@@ -63,3 +34,30 @@ fi
 if [ -n "${MAC_WORK}" ]; then
     export JAVA_HOME="/Library/Java/JavaVirtualMachines/zulu-17.jdk/Contents/Home"
 fi
+
+export PATH="\
+/data/data/com.termux/files/usr/bin:\
+/data/data/com.termux/files/usr/bin/applets:\
+/opt/homebrew/bin:\
+/opt/homebrew/opt/curl/bin:\
+/opt/homebrew/sbin:\
+/sbin:\
+/usr/local/bin:\
+/usr/local/go/bin:\
+/usr/local/opt/curl/bin:\
+/usr/local/sbin:\
+/usr/sbin:\
+${HOME}/.cargo/bin:\
+${HOME}/.gvm/go/bin:\
+${HOME}/.local/bin:\
+${HOME}/arcadia:\
+${HOME}/bin:\
+${HOME}/go/bin:\
+${PATH}\
+"
+
+export FPATH="\
+${HOME}/.zfunc:\
+${ZSH_CUSTOM_PLUGINS}/zsh-completions/src:\
+${FPATH}\
+"
